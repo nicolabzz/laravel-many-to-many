da178d3eed9f20f40645e205338c8ffb4c76911a
nicolabzz <nbuzzigoli@gmail.com>
2023-02-09 00:17:41 +0100

    .Post w/o lorempicsum

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -13,33 +13,20 @@ class PostSeeder extends Seeder
     public function run(Faker $faker)
     {
         $categories = Category::all('id')->all();
-        $tags = Tag::all()->pluck('id');
-        $tagCount = count($tags);
-
-        for ($i = 0; $i < 1000; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $title = $faker->words(rand(3, 7), true);
-            $number = rand(0, 276);
-            if ($number) {
-                $contents = new File(__DIR__ . '/../../storage/app/lorempicsum/picsum' . $number . '.jpg');
-                // $tmp_img_url = $faker->image();
-                $img_path = Storage::put('uploads', $contents);
-            } else {
-                $img_path = null;
-            }
+            // $loremImages = Storage::files('lorempicsum');
+            // $img_path = Storage::put('uploads', $faker->randomElement($loremImages));
 
-            $post = Post::create([
+            Post::create([
                 'category_id'   => $faker->randomElement($categories)->id,
                 'slug'          => Post::getSlug($title),
                 'title'         => $title,
                 'image'         => 'https://picsum.photos/id/' . rand(0, 1000) . '/500/400',
-                'uploaded_img'  => $img_path,
+                // 'uploaded_img'  => $img_path,
                 'content'       => $faker->paragraphs(rand(1, 10), true),
                 'excerpt'       => $faker->paragraph(),
             ]);
-
-
-            // questa riga scrive nella tabella ponte
-            $post->tags()->attach($faker->randomElements($tags, rand(0, ($tagCount > 10) ? 10 : $tagCount)));
         }
     }
 }
